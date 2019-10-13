66262df0079cff2e3f2990d6f6398a52d412a9f9
YEONJOONCYAN <yeonjoonjung1@gmail.com>
2019-10-12 19:02:58 -0700

    Test commit 2

--- app/src/main/java/com/example/dubhacksproject/MainActivity.java
+++ app/src/main/java/com/example/dubhacksproject/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //this is my test code
+        //this is my test code aha
     }
 }
